1dc42b99e6e6fbe489dfcee392f7b7055b05c8c1
kameshdangeti <kameshdangeti@gmail.com>
2024-03-05 16:59:07 +0530

    The code for is empty has been commited

--- codes/Is-Object-Empty.js
+++ codes/Is-Object-Empty.js
@@ -14,9 +14,10 @@ Explanation: The object has 2 key-value pairs so it is not empty.
  */
 
 var isEmpty = function (obj) {
-    if (Array.isArray(obj)) {
-        return obj.length === 0;
-    } else if (typeof obj === 'object' && obj !== null) {
+    if (Array.isArray(obj)) {      //The Array.isArray() static method determines 
+        return obj.length === 0;    //whether the passed value is an Array.
+    } else if (typeof obj === 'object' && obj !== null) {  //The typeof operator returns a string indicating 
+                                                            //the type of the operand's value.
         return Object.keys(obj).length === 0;
     }
     return false;
